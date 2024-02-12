41f8ce01d858f3443064885aa46d886b98e8f121
SergioPYD <puyod10@gmail.com>
2024-02-12 20:56:37 +0100

    croscors# Please enter the commit message for your changes. Lines starting

--- server/config/index.js
+++ server/config/index.js
@@ -23,7 +23,8 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: [FRONTEND_URL]
+      origin: process.env.FRONTEND_URL,
+      credentials: true
     })
   );
 
