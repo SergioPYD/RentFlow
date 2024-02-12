3819c0542153e27f39392771964bb9d01e20a8d1
SergioPYD <puyod10@gmail.com>
2024-02-12 20:33:08 +0100

    cors problem

--- server/config/index.js
+++ server/config/index.js
@@ -23,8 +23,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: process.env.FRONTEND_URL,
-      credentials: true
+      origin: [FRONTEND_URL]
     })
   );
 
