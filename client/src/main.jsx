c1562e1f78e0cc920c338f3aa04775971f8de239
SergioPYD <puyod10@gmail.com>
2024-02-16 12:44:02 +0100

    a

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { NextUIProvider } from '@nextui-org/react';
-import { AuthWrapper } from './context/auth.context.jsx';
-import App from './App.jsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from "react-router-dom";
+import { NextUIProvider } from "@nextui-org/react";
+import { AuthWrapper } from "./context/auth.context.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <NextUIProvider>
-      <BrowserRouter>
-        <AuthWrapper>
-          <main>
-            <App />
-          </main>
-        </AuthWrapper>
-      </BrowserRouter>
-    </NextUIProvider>
-  </React.StrictMode>
-);
+  <NextUIProvider>
+  <BrowserRouter>
+    <AuthWrapper>
+      <main>
+        <App />
+      </main>
+    </AuthWrapper>
+  </BrowserRouter>
+</NextUIProvider>
+)
